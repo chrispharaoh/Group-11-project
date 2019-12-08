2fd4e4411599bd0c7de737ee0e2448d499b93289
chrispharaoh <48133104+chrispharaoh@users.noreply.github.com>
2019-12-08 19:20:06 +0200

    Add files via upload

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,9 +6,7 @@ class Body extends Component{
     render(){
        return(
         <div className="body">
-
-            <button className="click">CLICK TO VIEW DHIS2 DASHBOARD</button>
-
+        
         </div>
     
     )}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+
+import { Icon } from 'react-icons-kit'
+import {fourUp} from 'react-icons-kit/iconic/fourUp'
+import {mail} from 'react-icons-kit/ikons/mail'
+import {bullhorn} from 'react-icons-kit/iconic/bullhorn'
 import '../App.css'
 import logo from './favicon.ico'
+
 
 
 class Header extends Component{
@@ -10,8 +16,12 @@ class Header extends Component{
         <div>
            <header>
                <img src={logo} className="logo" alt="logo"/>
-               <label className="ico">dhis2</label>
+               <label className="ico">dhis<span className="num">2</span></label>
                <label className="title">DHIS2 WEB PORTAL</label>
+               <div className="notif"><Icon icon={bullhorn}/></div>
+               <div className="mail"><Icon icon={mail}/></div>
+               <div className="fourup"><Icon icon={fourUp} /></div>
+               <input type="search" className="search" placeholder="Search"/>
            
 
             </header> 
